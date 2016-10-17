b5707e530c6e581d05fa3668a8fcb9194a056cb9
HengtaoDai <hengtao_dai@163.com>
2016-10-18 00:23:51 +0800

    Update HTSizeView.h

--- HTSizeView.h
+++ HTSizeView.h
@@ -1,6 +1,6 @@
 //
-//  WTSizeView.h
-//  WaiTanXS
+//  HTSizeView.h
+//  Test
 //
 //  Created by HengtaoDai on 16/10/9.
 //  Copyright © 2016年 HYcompany. All rights reserved.
@@ -8,18 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@protocol WTSizeViewDelegate <NSObject>
+@protocol HTSizeViewDelegate <NSObject>
 
 - (void)selectBtnInRow:(NSUInteger)iRow andIndex:(NSUInteger)index;
 
 @end
 
-@interface WTSizeView : UIView
+@interface HTSizeView : UIView
 
 @property (nonatomic, strong) NSDictionary *dicSize;
 @property (nonatomic, assign) CGFloat fHeight;
 @property (nonatomic, assign) NSUInteger iRow;     //第几个规格
-@property (nonatomic, weak) id<WTSizeViewDelegate> delegate;
+@property (nonatomic, weak) id<HTSizeViewDelegate> delegate;
 
 
 - (id)initWithFrame:(CGRect)frame;
